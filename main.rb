690e66bcd3a3bbb1879d32d96781c0280b78b227
Bradley Findeis <bradleyfindeis@Bradleys-MacBook.local>
2018-02-09 14:17:07 -0700

    second update

---
+++ main.rb
@@ -0,0 +1,11 @@
+# - User inputs question
+# - Computer outputs random answer
+# - User inputs "QUIT"
+# - Computer outputs a goodbye message and exits
+# - ability to add more answers:
+# - via easter egg question ("add_answers")
+# - do not let them add the same answer if the eight ball already has that answer
+# - ability to reset answers back to the original bank (hint: think arr.clone)
+# - via easter egg question ("reset_answers")
+# - ability to have eight ball print all answers
+# - via easter egg question ("print_answers")
