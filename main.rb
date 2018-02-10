111248b886697a9dca99fce5d61bb986070ed4b6
Bradley Findeis <bradleyfindeis@Bradleys-MacBook.local>
2018-02-09 17:21:37 -0700

    Fifth push

--- main.rb
+++ main.rb
@@ -7,14 +7,17 @@
 # - Make Print function cleaner
 require 'Pry'
 @answers = ["Sure Thang", "Not a Chance", "No Go", "I'll look into it", "Aww that's cute", "You Got This", "That's it"]
-
+@new_answers = @answers.clone
 def eight_ball
     puts "Please enter a question for our Magic Eightball to answer."
     # love_array = []
     question = gets.strip
     if question == "print"
-        print @answers
+        puts @answers
         exit 
+    elsif question == "reset"
+        @new_answers.delete_at(7)
+        puts @new_answers
     end
     # love_array << question
     # love_array.split(' ')
@@ -34,11 +37,11 @@ def eight_ball
     else 
        puts "Add an answer to the magic 8-Ball"
 
-        @add_answer = gets.to_s
-        @answers << @add_answer
-        if @add_answer == @answers [1..100]
-            puts "We already have that answer. Please try again:"
-            add
+        add_answer = gets.to_s
+        @answers << add_answer
+        # if add_answer == @answers [1..100]
+        #     puts "We already have that answer. Please try again:"
+            
     end
      
 end
@@ -46,10 +49,3 @@ end
 while true
 eight_ball
 end
-
-def verify
-    if @add_answer == @answers [1..100]
-        puts "We already have that answer. Please try again:"
-    
-    
-    end
